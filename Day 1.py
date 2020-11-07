fbc3ad42632e79aed688246d0bc53a6d60c4c684
Kazamasen <74109634+Kazamasen@users.noreply.github.com>
2020-11-07 23:48:09 +0530

    Day 1 assignment submitted

---
+++ Day 1.py
@@ -0,0 +1,26 @@
+# first question's answer is Option B RAINBOW
+
+# question 2:
+A = "LET'S UPGRADE"
+print(A)
+
+# plz remove '''' sign to use the program
+
+
+# question 3:
+'''''
+cp = int(input("CP:"))
+sp = int(input("SP:"))
+if cp < sp:
+    print("Profit")
+elif cp > sp:
+    print("Loss")
+else:
+    print("neither")
+'''''
+
+# question 4:
+e = int(input("€:"))
+r = 80
+c = r*e
+print("Rs", c)
